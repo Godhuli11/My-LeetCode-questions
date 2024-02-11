711083d5015a2712c7e9179bd6bdbe4cdffed8b3
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-02-12 00:02:56 +0530

    Time: 36 ms (61.52%), Space: 21.3 MB (40.90%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     string longestPrefix(string s) {
-         int n = s.size();
-        vector<int> lps(n, 0);
 
-        int len = 0;
-
-        for (int i = 1; i < n;) {
-            if (s[i] == s[len]) {
-                len++;
-                lps[i] = len;
+        int n = s.size();
+        vector<int> lps(n,0);
+        int j=0;
+        for(int i=1 ; i<n ;){
+            if(s[i]==s[j]){
+                j++;
+                lps[i]=j;
                 i++;
-            } else {
-                if (len != 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i] = 0;
+            }else{
+                if(j!=0){
+                    j = lps[j-1];
+                }else{
+                    lps[i]=0;
                     i++;
                 }
+                    
             }
-    }
-        
-        int longestLength = lps[n - 1];
-        return s.substr(0, longestLength);
+        }
+    int longestLength = lps[n-1];
+        return s.substr(0,longestLength);
     }
 };
