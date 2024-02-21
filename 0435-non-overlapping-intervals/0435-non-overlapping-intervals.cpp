55d240e647bb64ede2324d716666d8a5fca7790e
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-02-21 16:04:27 +0530

    Time: 255 ms (70.43%), Space: 93.2 MB (49.15%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,60 @@
+// class Solution {
+// public:
+//     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        
+//         int n = intervals.size();
+        
+//         if(n<=1) return 0;
+        
+//         sort(intervals.begin(),intervals.end());
+        
+//         int i=0;
+//         int j=1;
+        
+//         int ans=0;
+        
+//         while(i<n && j<n){
+//             if(intervals[i][1] <= intervals[j][0]){
+//                 i=j;
+//                 j++;
+//             }else{
+//                 ans++;
+//                 j++;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
+class Solution {
+public:
+    int eraseOverlapIntervals(std::vector<std::vector<int>>& intervals) {
+        int n = intervals.size();
+
+        if (n <= 1) {
+            return 0;  // No overlaps if there are 0 or 1 intervals
+        }
+
+        std::sort(intervals.begin(), intervals.end());
+
+        int i = 0;
+        int j = 1;
+        int ans = 0;
+
+        while (j < n) {
+            if (intervals[i][1] <= intervals[j][0]) {
+                i = j;
+                j++;
+            } else {
+                ans++;
+                // Erase the interval with the later end time
+                if (intervals[i][1] > intervals[j][1]) {
+                    i = j;
+                }
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
