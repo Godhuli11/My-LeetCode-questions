9496f0e406df17ac4917a453c55db69bf2d06c1e
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-02-21 22:09:37 +0530

    Time: 4 ms (58.71%), Space: 15.2 MB (19.58%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     TreeNode* solve(vector<int>& preorder , int start , int end , int size){
+//         if(start > end){
+//             return NULL;
+//         }
+//         TreeNode* root = new TreeNode(preorder[start]);
+//         int i = start;
+//         for( ; i < end ; i++){
+//             if(preorder[i]>preorder[start]){
+//                 break;
+//             }
+//         }
+//         end = i-1;
+//         root->left = solve(preorder , start+1 , end , size );
+//         root->right = solve(preorder , i , size , size);
+//         return root;
+//     }
+//     TreeNode* bstFromPreorder(vector<int>& preorder) {
+//         int n = preorder.size();
+//         return solve(preorder , 0 , n-1 , n);
+//     }
+// };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(vector<int>& preorder, int& start, int end, int size) {
+        if (start > end) {
+            return nullptr;
+        }
+
+        TreeNode* root = new TreeNode(preorder[start]);
+        int i = start;
+        for (; i <= end; i++) {
+            if (preorder[i] > preorder[start]) {
+                break;
+            }
+        }
+
+        start++;  // Move to the next element in preorder
+
+        root->left = solve(preorder, start, i - 1, size);
+        root->right = solve(preorder, start, end, size);
+
+        return root;
+    }
+
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int n = preorder.size();
+        int start = 0;
+        return solve(preorder, start, n - 1, n);
+    }
+};
