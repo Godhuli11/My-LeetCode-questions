91f928e0c7dc4c849f159dc3c002d0d24dfeb5a1
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-02-21 16:23:19 +0530

    Time: 246 ms (78.43%), Space: 93.4 MB (33.94%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,60 +1,67 @@
-// class Solution {
-// public:
-//     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        
-//         int n = intervals.size();
-        
-//         if(n<=1) return 0;
-        
-//         sort(intervals.begin(),intervals.end());
-        
-//         int i=0;
-//         int j=1;
-        
-//         int ans=0;
-        
-//         while(i<n && j<n){
-//             if(intervals[i][1] <= intervals[j][0]){
-//                 i=j;
-//                 j++;
-//             }else{
-//                 ans++;
-//                 j++;
-//             }
-//         }
-        
-//         return ans;
-//     }
-// };
 class Solution {
 public:
-    int eraseOverlapIntervals(std::vector<std::vector<int>>& intervals) {
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        
         int n = intervals.size();
-
-        if (n <= 1) {
-            return 0;  // No overlaps if there are 0 or 1 intervals
-        }
-
-        std::sort(intervals.begin(), intervals.end());
-
-        int i = 0;
-        int j = 1;
-        int ans = 0;
-
-        while (j < n) {
-            if (intervals[i][1] <= intervals[j][0]) {
-                i = j;
+        
+        if(n<=1) return 0;
+        
+        sort(intervals.begin(),intervals.end());
+        
+        int i=0;
+        int j=1;
+        
+        int ans=0;
+        
+        while(i<n && j<n){
+            if(intervals[i][1] <= intervals[j][0]){
+                i=j;
                 j++;
-            } else {
+            }
+            else if(intervals[i][1] <= intervals[j][1]){
                 ans++;
-                // Erase the interval with the later end time
-                if (intervals[i][1] > intervals[j][1]) {
-                    i = j;
-                }
+                j++;
+            }
+            else{
+                ans++;
+                i=j;
                 j++;
             }
         }
-
+        
         return ans;
-    }
+}
+    
 };
+// class Solution {
+// public:
+//     int eraseOverlapIntervals(std::vector<std::vector<int>>& intervals) {
+//         int n = intervals.size();
+
+//         if (n <= 1) {
+//             return 0;  // No overlaps if there are 0 or 1 intervals
+//         }
+
+//         std::sort(intervals.begin(), intervals.end());
+
+//         int i = 0;
+//         int j = 1;
+//         int ans = 0;
+
+//         while (j < n) {
+//             if (intervals[i][1] <= intervals[j][0]) {
+//                 i = j;
+//                 j++;
+//             } else {
+//                 ans++;
+//                 // Erase the interval with the later end time
+//                 if (intervals[i][1] > intervals[j][1]) {
+//                     i = j;
+//                 }
+//                 j++;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
