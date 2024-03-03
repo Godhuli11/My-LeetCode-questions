7dccc3e0d8572323b03e5eaba93eb22fc25f2b7f
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-03-03 14:25:53 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (50.39%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& nums, int maxSum){
+        
+        int partitions = 1;
+        
+        int subASum = 0;
+        
+        for(int i = 0 ; i<nums.size() ; i++){
+            
+            if( subASum + nums[i] <= maxSum){
+                subASum += nums[i];
+            }else{
+                partitions++;
+                subASum = nums[i];
+            }
+            
+        }
+        
+        return partitions;
+        
+    }
+    int splitArray(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        
+        int low = INT_MIN;
+        
+        for(int i=0 ; i<n ; i++){
+            low = max(nums[i],low);
+        }
+        
+        int high = 0;
+        
+        for(int i=0 ; i<n ; i++){
+            high += nums[i];
+        }
+        
+        while(low < high ){
+            
+            int mid = (low + high) / 2;
+            
+            int partitions = countPartitions(nums,mid);
+            
+            if( partitions > k){
+                low = mid + 1;
+            }else{ //partitions <= k
+                
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+};
