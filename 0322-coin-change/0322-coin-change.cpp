e017cf3140fb8b426fc2d9fff8ed5ed015de425b
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-03-03 23:46:07 +0530

    Time: 20 ms (88.43%), Space: 16.2 MB (52.40%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-     int coinChangeRecursiveHelper(const vector<int>& coins, int amount,vector<int> &dp) {
-        if (amount == 0) {
-            return 0;
-        }
-          
-        if(dp[amount]!=-1) return dp[amount];
-        int minCoins = INT_MAX;
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX); // Initialize the table with INT_MAX
+        dp[0] = 0; // Base case: zero coins needed to make amount 0
 
-        for (int coin : coins) {
-            if (coin <= amount) {
-                int subproblem = coinChangeRecursiveHelper(coins, amount - coin,dp);
-                if (subproblem != INT_MAX) {
-                    minCoins = min(minCoins, 1 + subproblem);
+        for (int i = 1; i <= amount; ++i) {
+            for (int coin : coins) {
+                if (coin <= i && dp[i - coin] != INT_MAX) {
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
                 }
             }
         }
-         dp[amount] = minCoins;
-        return dp[amount];
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
-           int result = coinChangeRecursiveHelper(coins, amount,dp);
-        return (result == INT_MAX) ? -1 : result;
+
+        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
     }
 };
