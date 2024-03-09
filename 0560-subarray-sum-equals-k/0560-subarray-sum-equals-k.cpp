ecad80b3435ebb2074789141bc38a90963b4b5a3
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-03-09 16:21:58 +0530

    Time: 90 ms (39.69%), Space: 46.2 MB (40.01%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,29 +1,62 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         map<int, int> sumToCount;
+
+//         int ans = 0;
+//         int sum = 0;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             sum += nums[i];
+
+//             // Check if there exist a subarray with sum = k up to the current index
+//             if (sum == k) {
+//                 ans++;
+//             }
+
+//             // Check if there exist a subarray with sum = k by subtracting cumulative sum up to a previous index
+//             int temp = sum - k;
+//             if (sumToCount.find(temp) != sumToCount.end()) {
+//                 ans += sumToCount[temp];
+//             }
+
+//             // Update the frequency of the current cumulative sum
+//             sumToCount[sum]++;
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        map<int, int> sumToCount;
-
+        
+        map<int,int> sumToInd;
+        
         int ans = 0;
-        int sum = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
+        
+        int sum=0 ;
+        
+        for(int i=0 ; i<nums.size() ; i++){
             sum += nums[i];
-
-            // Check if there exist a subarray with sum = k up to the current index
-            if (sum == k) {
+            int temp = sum - k;
+            if(temp == 0){
                 ans++;
             }
-
-            // Check if there exist a subarray with sum = k by subtracting cumulative sum up to a previous index
-            int temp = sum - k;
-            if (sumToCount.find(temp) != sumToCount.end()) {
-                ans += sumToCount[temp];
-            }
-
-            // Update the frequency of the current cumulative sum
-            sumToCount[sum]++;
+            // if(temp > 0){
+            //     if(sumToInd.find(temp) != sumToInd.end()){
+            //         ans+=sumToInd[temp];
+            //     }
+            // }
+             
+                if(sumToInd.find(temp) != sumToInd.end()){
+                    ans+=sumToInd[temp];
+                }
+           
+           
+            sumToInd[sum]++;
         }
-
+        
         return ans;
     }
 };
