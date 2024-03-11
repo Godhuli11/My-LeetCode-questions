0897a83a5caac39c5a445872eb1b4d5cae297a20
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-03-11 22:50:29 +0530

    Time: 5 ms (19.38%), Space: 7.3 MB (34.34%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        // Check if n is positive and has only one set bit
-        return n > 0 && (n & (n - 1)) == 0;
+        if (n > 0 && (n & (n - 1)) == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
