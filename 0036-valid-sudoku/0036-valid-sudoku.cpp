c6e60b729e2f5546fb56b0b3463c353650ce635d
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-03-30 16:35:34 +0530

    Time: 26 ms (9.38%), Space: 25 MB (18.04%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -10,9 +10,9 @@ public:
                     continue;
                 }
                 
-                string row = string(1,mat[i][j])+"_ROW_"+ to_string(i);
-                string col = string(1,mat[i][j])+"_COL_"+ to_string(j);
-                string box = string(1,mat[i][j])+"_BOX_"+ to_string(i/3) + "__" + to_string(j/3);
+                string row = to_string(mat[i][j]) +"_ROW_"+ to_string(i);
+                string col =to_string(mat[i][j]) +"_COL_"+ to_string(j);
+                string box = to_string(mat[i][j])+"_BOX_"+ to_string(i/3) + "__" + to_string(j/3);
                 if(st.find(row)!=st.end()||st.find(col)!=st.end()||st.find(box)!=st.end()){
                     return false;
                 }
