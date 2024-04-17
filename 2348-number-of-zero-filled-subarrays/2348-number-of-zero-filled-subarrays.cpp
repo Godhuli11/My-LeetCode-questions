006fade4d0a19dd98b438d555b314d46f781b7cf
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-04-17 12:09:22 +0530

    Time: 108 ms (81.00%), Space: 109.8 MB (85.92%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,60 +1,31 @@
-// class Solution {
-// public:
-//     long long zeroFilledSubarray(vector<int>& nums) {
-        
-//         int n = nums.size();
-        
-//         int i=0;
-        
-//         long  long res = 0;
-        
-//         while(i<n){
-//             long long l = 0;
-            
-//             if(nums[i]==0){
-                 
-//                 while(l<n && nums[i]==0){
-//                 l++;
-//                 i++;
-//             }
-            
-//             }else{
-//                 i++;
-//             }
-           
-//             res += l*(l+1)/2;
-            
-//         }
-        
-//         return res;
-//     }
-// };
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long result = 0;
         
         int n = nums.size();
         
-        int i = 0;
-        while(i < n) {
+        int i=0;
+        
+        long  long res = 0;
+        
+        while(i<n){
+            long long l = 0;
             
-            long long zeros = 0;
-            
-            if(nums[i] == 0) {
-                
-                while(i < n && nums[i] == 0) {
-                    i++;
-                    zeros++;
-                }
-                
-            } else {
-                    i++;
+            if(nums[i]==0){
+                 
+                while(i<n && nums[i]==0){
+                l++;
+                i++;
             }
-            result += (zeros)*(zeros+1)/2;
+            
+            }else{
+                i++;
+            }
+           
+            res += l*(l+1)/2;
+            
         }
         
-        return result;
-        
+        return res;
     }
 };
