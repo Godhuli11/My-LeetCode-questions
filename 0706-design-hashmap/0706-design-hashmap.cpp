80dcc0d649dcec05f67e2492a5c463d348859e4a
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-07-09 03:55:13 +0530

    Time: 98 ms (69.53%), Space: 65.9 MB (55.60%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -8,7 +8,7 @@ public:
     
     void put(int key, int value) {
         int bucket_no = key%size;
-        auto& chain = buckets[bucket_no];
+        auto& chain = buckets[bucket_no]; //auto& is imp cuz whatver change we are making should be added to the actual list and not just in the copy of it thats why pass by ref
         for(auto &it:chain){
             if(it.first == key){
                 it.second = value;
@@ -20,7 +20,7 @@ public:
     
     int get(int key) {
         int bucket_no = key%size;
-        auto& chain = buckets[bucket_no];
+        auto& chain = buckets[bucket_no]; //auto& is imp
         for(auto &it:chain){
             if(it.first == key){
                 return it.second;
@@ -35,12 +35,13 @@ public:
         auto& chain = buckets[bucket_no];
         
         for(auto it = chain.begin() ; it != chain.end() ;){
-            if(it->first == key){
+            if(it->first == key){ //it is a ptr not a pair object and we need it
+                                  // to be a ptr so that we can delete it
                 chain.erase(it);
                 return;
             }
             
-            it++;
+            it++;;
         }
     }
 };
