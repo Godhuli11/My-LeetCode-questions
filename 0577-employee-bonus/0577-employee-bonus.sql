2cd47e082d641aa124bfb3446ed58b7d827d2509
Godhuli Bhattacharya <85281252+Godhuli11@users.noreply.github.com>
2024-08-13 00:12:35 +0530

    Time: 1219 ms (12.25%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -5,8 +5,9 @@
 # Bonus as b
 # on e.empId = b.empId
 # where b.bonus <= 1000 or b.bonus is NULL ;
+
 SELECT e.name, b.bonus 
-FROM Employee e 
-LEFT JOIN Bonus b 
+FROM Employee as e 
+LEFT JOIN Bonus as b 
 ON e.empId = b.empId
 WHERE b.bonus < 1000 OR b.bonus IS NULL;
